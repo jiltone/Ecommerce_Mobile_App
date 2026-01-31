01ecc25e47cc41a884537e1e10e7fd2bb5a1a45e
jiltone <93777613+jiltone@users.noreply.github.com>
2026-01-31 21:31:41 +0530

    add common file

---
+++ jiltone_store/lib/navigation_menu.dart
@@ -0,0 +1,54 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:shopping_store/features/personalization/screens/settings/settings.dart';
+import 'package:shopping_store/features/shop/screens/store/store.dart';
+import 'package:shopping_store/features/shop/screens/wishlist/wishlist.dart';
+import 'package:shopping_store/utils/constants/colors.dart';
+import 'package:shopping_store/utils/helpers/helper_functions.dart';
+
+import 'features/shop/screens/home/home.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    final dark = HkHelperFunctions.isDarkMode(context);
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        ()=> NavigationBar(
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (value) => controller.selectedIndex.value = value,
+          backgroundColor: dark ? HkColors.dark : HkColors.light,
+          indicatorColor: dark ? HkColors.light.withOpacity(0.1) : HkColors.dark.withOpacity(0.1),
+          destinations: const [
+            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
+            NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile')
+          ],
+        ),
+      ),
+      body: Obx(()=>  controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
+
+
+class NavigationController extends GetxController{
+
+  final RxInt selectedIndex = 0.obs;
+
+  final screens = [
+    const HomeScreen(),
+    const StoreScreen(),
+    const FavouriteScreen(),
+    const SettingsScreen()
+  ];
+}
